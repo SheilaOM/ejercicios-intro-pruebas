4ab0414df760e8fabfd3220f3b6e511510cc8add
SheilaOM <sheila.oliva14@gmail.com>
2017-11-01 13:15:15 +0100

    Nuevo test

--- code/CountPositive/CountPositiveTest.java
+++ code/CountPositive/CountPositiveTest.java
@@ -5,9 +5,15 @@ import java.util.*;
 public class CountPositiveTest
 {
    // this test fails!
-   @Test public void arrayContainsZeroes() 
+   @Test public void arrayContainsZeroes()
    {
       int arr[] = {-4, 2, 0, 2};
       assertEquals("Array contains zeroes", 2, CountPositive.countPositive(arr));
+   }
+
+   @Test public void arrayNoContainsZeroes()
+   {
+      int arr[] = {1, -2, 3};
+      assertEquals("Array no contains zeroes", 2, CountPositive.countPositive(arr));
    }
 }
