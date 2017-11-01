7b563df7f4105ddb58905c20ea60a0b212f212e5
SheilaOM <sheila.oliva14@gmail.com>
2017-11-01 14:49:38 +0100

    Nuevo test

--- code/OddOrPos/OddOrPosTest.java
+++ code/OddOrPos/OddOrPosTest.java
@@ -5,9 +5,14 @@ import java.util.*;
 public class OddOrPosTest
 {
    // this test fails!
-   @Test public void negativeOddNumbers() 
+   @Test public void negativeOddNumbers()
    {
       int arr[] = {-3, -2, 0, 1, 4};
       assertEquals("Negative odd numbers in array", 3, OddOrPos.oddOrPos(arr));
+   }
+   @Test public void noNegativeOddNumbers()
+   {
+      int arr[] = {-4, -2, 0, 1, 3, 4};
+      assertEquals("No negative odd numbers in array", 3, OddOrPos.oddOrPos(arr));
    }
 }
